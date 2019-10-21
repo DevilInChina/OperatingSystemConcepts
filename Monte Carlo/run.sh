1440f9c2298a1b0e88dbc62aeeba5d5afd3b496f
DevilInChina <905320627@qq.com>
2019-10-21 20:05:50 +0800

    add O3 optimize

--- Monte Carlo/run.sh
+++ Monte Carlo/run.sh
@@ -1,5 +1,5 @@
 cc=c++
-	${cc} main.cpp -o main -lpthread
-	${cc} 'test.cpp' -o ts -lpthread
+	${cc} main.cpp -o main -lpthread -O3
+	${cc} 'test.cpp' -o ts -lpthread -O3
 	./ts $1 $2 $3 data.txt
 	rm data.txt
